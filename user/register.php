947a1a4cba3f9a30f8ab5687d539a624966a9946
Mathias Collas-Jourdan <mathias.collas@gmail.com>
2023-05-26 14:00:12 +0200

    remise en forme des balises

--- user/register.php
+++ user/register.php
@@ -17,41 +17,36 @@
                 unset($_SESSION['errors']);
             }
             ?>
-
-            <form action="../core/registerUser.php" method="POST">
-                <input class="form-control" name="lastname" id="lastname" placeholder="Votre Nom" required="required">
-                <input class="form-control" name="firstname" id="firstname" placeholder="Votre prénom" required="required">
-
-                <label for="birthday" class="form-label">
-                    Votre date de naissance :
-                </label>
-
-                <input class="form-control" type="date" id="birthday" name="birthday"
-                    placeholder="Votre date de naissance" required="required">
         </div>
-        <div class="col-md-6">
-            <input class="form-control" type="email" name="email" placeholder="Votre email" required="required">
 
-            <input class="form-control" type="password" name="pwd" id="pwd" placeholder="Votre mot de passe" required="required">
+        <form action="../core/registerUser.php" method="POST">
+            <input class="form-control" name="lastname" id="lastname" placeholder="Votre Nom" required="required">
+            <input class="form-control" name="firstname" id="firstname" placeholder="Votre prénom" required="required">
 
-            <input class="form-control" type="password" name="pwdConfirmation" id="pwdConfirmation"
+            <label for="birthday" class="form-label">
+                Votre date de naissance :
+            </label>
+            <input class="form-control" type="date" id="birthday" name="birthday"
+            placeholder="Votre date de naissance" required="required">
+
+            <div class="col-md-6">
+                <input class="form-control" type="email" name="email" placeholder="Votre email" required="required">
+
+                <input class="form-control" type="password" name="pwd" id="pwd" placeholder="Votre mot de passe" required="required">
+
+                <input class="form-control" type="password" name="pwdConfirmation" id="pwdConfirmation"
                 placeholder="Confirmez votre mot de passe" required="required">
 
-            <label for="cgu">J'accepte les CGU
-                <a href="cgu.php" target="_blank">(Conditions Générales d'Utilisation)</a>
-
-            </label>
-            <input class="form-check-input" type="checkbox" name="cgu" id="cgu">
-            <label>
-                <button class="btn btn-primary mb-4">S'inscrire</button>
-            </label>
-            </form>
-        </div>
+                <label for="cgu">J'accepte les CGU
+                    <a href="cgu.php" target="_blank">(Conditions Générales d'Utilisation)</a>
+                </label>
+                <input class="form-check-input" type="checkbox" name="cgu" id="cgu">
+                <label>
+                    <button class="btn btn-primary mb-4">S'inscrire</button>
+                </label>
+            </div>  
+        </form>
     </div>
 </div>
-<div class="alert alert-info">
-    <p>
-        Vous êtes déjà inscrit ? <a href="../login.php"><u>Connectez-vous</u></a>
-    </p>
-</div>
+
 <?php include('../template/footer.php'); ?>
