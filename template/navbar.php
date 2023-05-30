9397784bee622a711f7df35cd4fc5e0f57f604db
Lucas Verrecchia <120099311+LucasVerrecchia@users.noreply.github.com>
2023-05-30 11:49:35 +0200

    Update navbar.php

--- template/navbar.php
+++ template/navbar.php
@@ -1,16 +1,16 @@
 <nav>
     <ul>
-        <li><a class="navbar-link" href="/Meca-Chrysalide-main/index.php">Home</a></li>
-        <li><a class="navbar-link" href="/Meca-Chrysalide-main/apropos.php">A Propos</a></li>
-        <li><a class="navbar-link" href="/Meca-Chrysalide-main/boutique.php">Boutique</a></li>
-        <li><a class="navbar-link" href="/Meca-Chrysalide-main/events.php">Evènements</a></li>
+        <li><a class="navbar-link" href="/MecaChrysalide/index.php">Home</a></li>
+        <li><a class="navbar-link" href="/MecaChrysalide/apropos.php">A Propos</a></li>
+        <li><a class="navbar-link" href="/MecaChrysalide/boutique.php">Boutique</a></li>
+        <li><a class="navbar-link" href="/MecaChrysalide/events.php">Evènements</a></li>
         <?php if (!isConnected()) { ?>
-            <li><a class="navbar-link" href="/Meca-Chrysalide-main/user/register.php">S'inscrire</a></li>
-            <li><a class="navbar-link" href="/Meca-Chrysalide-main/user/login.php">Se connecter</a></li>
+            <li><a class="navbar-link" href="/MecaChrysalide/user/register.php">S'inscrire</a></li>
+            <li><a class="navbar-link" href="/MecaChrysalide/user/login.php">Se connecter</a></li>
         <?php } else { ?>
-            <li><a class="navbar-link" href="/Meca-Chrysalide-main/user/logout.php">Se déconnecter</a></li>
+            <li><a class="navbar-link" href="/MecaChrysalide/user/logout.php">Se déconnecter</a></li>
         <?php } ?>
-        <li><a class="navbar-link" href="/Meca-Chrysalide-main/backoffice.php">Panneau de configuration
+        <li><a class="navbar-link" href="/MecaChrysalide/backoffice.php">Panneau de configuration
                 administrateur</a></li>
     </ul>
 </nav>
