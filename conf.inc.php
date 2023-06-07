2aa7398b80c44b247458907345ade58d20d59e85
Mathias Collas-Jourdan <mathias.collas@gmail.com>
2023-06-07 23:04:46 +0200

    a faire : création user pour BDD voir comm dans fichier

--- conf.inc.php
+++ conf.inc.php
@@ -4,8 +4,8 @@
 define("DB_HOST","localhost");
 define("DB_NAME","meca");
 define("DB_PORT","3306");
-define("DB_USER","root");
-define("DB_PWD","");
+define("DB_USER","root");   //à modifier 
+define("DB_PWD","");        //ajouter un mdp
 define("DB_PREFIX","lsm3_");
 /*
 define("DB_HOST", "54.36.183.20");
