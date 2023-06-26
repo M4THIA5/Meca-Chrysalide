e8a9816af34ff1858422c684cc1ccdc4dd427c19
Mathias Collas-Jourdan <mathias.collas@gmail.com>
2023-06-26 18:23:31 +0200

    ajout easter egg

--- template/footer.php
+++ template/footer.php
@@ -3,9 +3,22 @@
     <a href="https://www.facebook.com/">Facebook</a>
     <a href="https://www.instagram.com/meca_chrysalide/" target="_blank">Instagram</a>
 
-    <span> © 2023 Meca-Chrysalide par Félix Boyer </span>
+    <span> 
+        © 2023 Meca-Chrysalide par Félix Boyer 
+    </span>
+    <img id="easterEgg" src="/MecaChrysalide/assets/img/meca-cursor.png" alt="Logo Meca-Chrysalide" style="height: 5%; width: 5%;">
 
 </footer>
+<script>
+    var easterEgg = document.getElementById('easterEgg');
+    var body = document.body;
+
+    easterEgg.addEventListener('dblclick', function() {
+        body.classList.toggle('clicked');
+    });
+
+
+</script>
 </body>
 
 </html>
